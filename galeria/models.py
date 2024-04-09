0616e7166eeb570ecb6f72e183eb2d4e22b33e25
gwpoliveira <gwpoliveira@gmail.com>
2024-04-09 13:35:10 -0300

    base

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
-# Create your models here.
+from datetime import datetime
+class Fotografia(models.Model):
+    OPCOES_CATEGORIAS = [
+        ("Nebulosa", "Nebulosa"),
+        ("Estrela", "Estrela"),
+        ("GALÁXIA", "Galáxia"),
+        ("PLANETA", "Planeta")
+    ]
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legeda = models.CharField(max_length=200, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIAS, default="")
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
+    publicado = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+         return self.nome
